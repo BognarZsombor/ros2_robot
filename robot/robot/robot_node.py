c3274124fd57a8bfb355f7b923a8c2b538807d62
BognarZsombor <bzsombi99@gmail.com>
2023-10-04 11:06:09 +0200

    beszéd kész van a bill és a robot között és kimegy egy pose üzenet a robottól, valszeg rossz értékekkel(rot to quat hibás)

--- robot/robot/robot_node.py
+++ robot/robot/robot_node.py
@@ -1,11 +1,22 @@
 import rclpy
 from rclpy.node import Node
 
+import math
+from scipy.spatial.transform import Rotation
+
 from std_msgs.msg import String
 from geometry_msgs.msg import Pose, Point, Quaternion
 
 
 class Robot(Node):
+
+    R = 1.0
+    L = 3.0
+    x = 0.0
+    y = 0.0
+    theta = 0.0
+    wr = 0.0
+    wl = 0.0
 
     def __init__(self):
         super().__init__('robot')
@@ -22,27 +33,78 @@ class Robot(Node):
         self.i = 0
 
     def listener_callback(self, msg):
-        self.get_logger().info(f"Key pressed: {msg.data}")
+        if (msg.data == "\'w\'"):
+            self.wr = 1.0
+            self.wl = 1.0
+        elif (msg.data == "\'s\'"):
+            self.wr = -1.0
+            self.wl = -1.0
+        elif (msg.data == "\'a\'"):
+            self.wr = 1.0
+            self.wl = -1.0
+        elif (msg.data == "\'d\'"):
+            self.wr = -1.0
+            self.wl = 1.0
+
+        A = [[1.0, 0.0, 0.0],
+             [0.0, 1.0, 0.0],
+             [0.0, 0.0, 1.0]]
+        
+        xt = [[self.x],
+              [self.y],
+              [self.theta]]
+        
+        B = [[math.cos(self.theta), 0,0],
+             [math.sin(self.theta), 0,0],
+             [0.0, 1.0]]
+        
+        v = (self.wr + self.wl) * self.R / 2
+        w = (self.wr - self.wl) * self.R / 2
+
+        ut = [[v],
+              [w]]
+        
+        Ax = self.matmul(A, xt)
+        Bu = self.matmul(B, ut)
+
+        result = self.matadd(Ax, Bu)
+
+        self.x = result[0][0]
+        self.y = result[1][0]
+        self.theta = result[2][0]
+
+
+    def matadd(self, A, B):
+        return [[A[i][j] + B[i][j]  for j in range(len(A[0]))] for i in range(len(A))]
+
+    def matmul(self, A, B):
+        return [[sum(a*b for a,b in zip(A_row, B_col)) for B_col in zip(*B)] for A_row in A]
+        
+
 
     def timer_callback(self):
         msg = Pose()
         msg_point = Point()
         msg_quater = Quaternion()
 
-        msg_point.x = 0.0
-        msg_point.y = 0.0
+        msg_point.x = self.x
+        msg_point.y = self.y
         msg_point.z = 0.0
 
-        msg_quater.x = 0.0
-        msg_quater.y = 0.0
-        msg_quater.z = 0.0
-        msg_quater.w = 0.0
+        rot = Rotation.from_euler('xyz', [0, 0, self.theta], degrees=True)
+        rot_quat = rot.as_quat()
+
+        msg_quater.x = rot_quat[0]
+        msg_quater.y = rot_quat[1]
+        msg_quater.z = rot_quat[2]
+        msg_quater.w = rot_quat[3]
 
         msg.position = msg_point
         msg.orientation = msg_quater
 
+        self.get_logger().info(f"Result: {msg}")
+
         self.publisher_.publish(msg)
-        self.get_logger().info(f"Publishing: {msg}")
 
 
 def main(args=None):
